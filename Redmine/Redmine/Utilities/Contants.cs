af978c9bce6f853937680321d62edcbf0f607ff0
Erick Jimenez <erick.jimenez@unosquare.com>
2021-02-02 14:45:59 -0600

    Removed username and password

--- Redmine/Redmine/Utilities/Contants.cs
+++ Redmine/Redmine/Utilities/Contants.cs
@@ -2,11 +2,12 @@
 {
     public static class Constants
     {
+        //Change below urls accordingly based on your custom issues on your owned projects
         public static string BaseURL { get; } = "https://dev.unosquare.com/redmine/login?back_url=https%3A%2F%2Fdev.unosquare.com%2Fredmine%2F";
         public static string TaskURLAutomation { get; } = "https://dev.unosquare.com/redmine/issues/59204";
         public static string TaskURLNonProject { get; } = "https://dev.unosquare.com/redmine/projects/axos-bank-qa-automation-non-project-work";
-        public static string Username { get; } = "erick.jimenez@unosquare.com";
-        public static string Password { get; } = "luna77E!";
+        public static string Username { get; } = "someone@unosquare.com";
+        public static string Password { get; } = "password";
         public static string CommentsMeeting { get; } = "UDB Automation Meeting";
         public static string CommentsDesign { get; } = "UDB Script Development";
         public static string CommentsTesting { get; } = "UDB Test Case Execution";
