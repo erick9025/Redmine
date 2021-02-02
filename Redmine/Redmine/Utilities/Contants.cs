95e84e7c23ad360f8e00bc68c593290719d86a4c
Erick Jimenez <erick.jimenez@unosquare.com>
2021-02-02 15:08:01 -0600

    REmoved pwd

--- Redmine/Redmine/Utilities/Contants.cs
+++ Redmine/Redmine/Utilities/Contants.cs
@@ -5,8 +5,8 @@
         public static string BaseURL { get; } = "https://dev.unosquare.com/redmine/login?back_url=https%3A%2F%2Fdev.unosquare.com%2Fredmine%2F";
         public static string TaskURLAutomation { get; } = "https://dev.unosquare.com/redmine/issues/59204";
         public static string TaskURLNonProject { get; } = "https://dev.unosquare.com/redmine/projects/axos-bank-qa-automation-non-project-work";
-        public static string Username { get; } = "erick.jimenez@unosquare.com";
-        public static string Password { get; } = "luna77E!";
+        public static string Username { get; } = "@unosquare.com";
+        public static string Password { get; } = "pwd";
         public static string CommentsMeeting { get; } = "UDB Automation Meeting";
         public static string CommentsDesign { get; } = "UDB Script Development";
         public static string CommentsTesting { get; } = "UDB Test Case Execution";
